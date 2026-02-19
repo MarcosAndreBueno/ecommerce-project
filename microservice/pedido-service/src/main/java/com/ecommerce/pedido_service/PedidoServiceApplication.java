63cb0d29f61f946a872af8fb52f3a8dff3dbad21
Marcos Bueno <www.bueno@yahoo.com>
2026-02-19 07:02:51 -0300

    Fix: jpa auditing

--- microservice/pedido-service/src/main/java/com/ecommerce/pedido_service/PedidoServiceApplication.java
+++ microservice/pedido-service/src/main/java/com/ecommerce/pedido_service/PedidoServiceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.pedido_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PedidoServiceApplication {
 
     public static void main(String[] args) {
